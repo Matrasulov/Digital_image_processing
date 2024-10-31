6ee31533de200323b04109dfbf89957107a806f8
Akbarjon <76453238+Matrasulov@users.noreply.github.com>
2024-10-31 23:12:56 +0900

    Add files via upload

---
+++ face_detection_VJ.py
@@ -0,0 +1,31 @@
+import cv2
+
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('faces.xml')
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30))
+
+    # Draw rectangles around detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the frame with detected faces
+    cv2.imshow('Face Detection', frame)
+
+    # Break the loop when 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
